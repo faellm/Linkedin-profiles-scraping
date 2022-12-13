a4cbe463aab74d058ae3796746c29673391e6cb1
faellm <rafaellara1405@gmail.com>
2022-12-13 08:52:29 -0300

    'nao esta salvando na planilha'

---
+++ data.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.by import By
+
+
+
+
+#id e class de componentes html
+
+input_user = 'session_key' #id
+input_pass = 'session_password' #id
+
+btn_entrar = 'sign-in-form__submit-button' #class
+
+btn_public = '//*[@id="ember77"]/div/div/div[1]/div[2]' #xpath
+
+btn_pesquisar = 'search-global-typeahead__collapsed-search-button' #class
+input_pesquisar = 'search-global-typeahead__input'
+
+look_more_people = 'search-results__cluster-bottom-banner artdeco-button artdeco-button--tertiary artdeco-button--muted'
